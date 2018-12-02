4c76810d0b49b86ef2294f0db57e6234dbddd140
José Pedro Ferreira <zepedroferreira11@gmail.com>
2018-12-02 14:12:41 +0000

    Replace regsubset.R

--- Parte 2/Selecção do modelo/regsubset.R
+++ Parte 2/Selecção do modelo/regsubset.R
@@ -2,3 +2,12 @@ library(leaps)
 
 selecao = regsubsets(diabetesB~.-glyhb,data=nossoDiabetes,nvmax=16)
 summary(selecao)
+
+selecao = regsubsets(diabetesB~.-glyhb,data=nossoDiabetes,nvmax=16, method = "forward")
+summary(selecao)
+
+selecao = regsubsets(diabetesB~.-glyhb,data=nossoDiabetes,nvmax=16, method = "backward")
+summary(selecao)
+
+selecao = regsubsets(diabetesB~.-glyhb,data=nossoDiabetes,nvmax=16, method = "seqrep")
+summary(selecao)
