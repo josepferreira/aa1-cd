0da749e22457a2c1ad775496443baea403f7ccfb
José Pedro Ferreira <zepedroferreira11@gmail.com>
2018-12-10 21:36:54 +0000

    Replace testeKNNNossa.R

--- Parte 2/Selecção do modelo/testeKNNNossa.R
+++ Parte 2/Selecção do modelo/testeKNNNossa.R
@@ -44,4 +44,64 @@ resultadoKnnCV5 = geraKNNCV(treino,treino$diabetesB,1,20)
 
 melhorK5 = melhorIndice(resultadoKnnCV5)
 
-## o melhor continua a ser 1 var com k = 1 ##
+## o melhor é com 3 e k=10 ##
+
+## ordena todos para selecionar os 5 melhores ##
+valores = rep(1,dim(resultadoKnnCV1)[1])
+res1 = data.frame(resultadoKnnCV1,valores)
+valores = rep(2,dim(resultadoKnnCV1)[1])
+res2 = data.frame(resultadoKnnCV2,valores)
+valores = rep(3,dim(resultadoKnnCV1)[1])
+res3 = data.frame(resultadoKnnCV3,valores)
+valores = rep(4,dim(resultadoKnnCV1)[1])
+res4 = data.frame(resultadoKnnCV4,valores)
+valores = rep(5,dim(resultadoKnnCV1)[1])
+res5 = data.frame(resultadoKnnCV5,valores)
+
+todos = rbind(res1,res2,res3,res4,res5)
+todosC = consomeValores(todos)
+todosOrd = todosC[order(-todosC$valor),]
+
+
+## Teste com BD dividida entre treino e teste so para ver ##
+set.seed(1)
+dadosTreino = sample(403,403/4)
+diabetesTreino = nossoDiabetes[-dadosTreino, ]
+diabetesTeste = nossoDiabetes[dadosTreino, ]
+dim(diabetesTreino[complete.cases(diabetesTreino),])
+dim(diabetesTeste[complete.cases(diabetesTeste),])
+
+## Teste com 1 var do regsubsets ##
+treino = dplyr::select(diabetesTreino, diabetesB,stab.glu)
+teste = dplyr::select(diabetesTeste, diabetesB,stab.glu)
+treino = treino[complete.cases(treino),]
+teste = teste[complete.cases(teste),]
+treino = normaliza(treino)
+teste = normaliza(teste)
+
+resultadoKnn1 = geraKNN(treino[,-1],treino$diabetesB,teste[,-1],teste$diabetesB,1,20)
+
+melhorKnn1 = melhorIndice(resultadoKnn1)
+
+set.seed(1)
+knn.pred = knn(data.frame(treino$stab.glu),data.frame(teste$stab.glu),treino$diabetesB,k=1)
+table(knn.pred,teste$diabetesB)
+## Teste com 2 vars do regsubsets ##
+treino = dplyr::select(nossoDiabetes, diabetesB,stab.glu, age)
+treino = treino[complete.cases(treino),]
+
+resultadoKnn2 = geraKNN(treino,treino$diabetesB,1,20)
+
+melhorKnn2 = melhorIndice(resultadoKnn2)
+
+## Teste com 3 vars do regsubsets ##
+treino = dplyr::select(nossoDiabetes, diabetesB,stab.glu, age, bp.1s)
+treino = treino[complete.cases(treino),]
+
+resultadoKnn3 = geraKNN(treino,treino$diabetesB,1,20)
+
+melhorKnn3 = melhorIndice(resultadoKnn3)
+
+set.seed(1)
+table(knn.cv(treino, treino$diabetesB, k = 1, l = 0, prob = FALSE, use.all = TRUE),
+      treino$diabetesB)
