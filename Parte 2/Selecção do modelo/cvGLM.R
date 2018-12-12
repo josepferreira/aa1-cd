3443940304ca2d03ba3daf3e462e86a1ef8d2c21
Carlos <carlos_jose_lima@hotmail.com>
2018-12-12 10:01:25 +0000

    Replace cvGLM.R

--- Parte 2/Selecção do modelo/cvGLM.R
+++ Parte 2/Selecção do modelo/cvGLM.R
@@ -32,12 +32,26 @@ attach(dados2)
 ## Melhor erro ponderaro: 3.51 , th:0.4
 set.seed(1)
 thr = graficoThreshold(0,1,0.1,glmCV,diabetesB~stab.glu,dados2)
+glm.fit=glm(diabetesB~stab.glu,data=dados,family=binomial)
+plot(glm.fit)
+
 melhor = melhorIndice(thr)
 
 ## Melhor erro ponderaro: 3.51 , th:0.4
 set.seed(1)
 thr = graficoThreshold(0,1,0.1,glmCV,diabetesB~stab.glu+age,dados2)
 melhor = melhorIndice(thr)
+
+## 1p - Melhor erro ponderaro: 3.55 , th:0.4
+set.seed(1)
+thr = graficoThreshold(0,1,0.01,glmCV,diabetesB~stab.glu+age+stab.glu:age,dados2)
+melhor = melhorIndice(thr)
+thr[40,]
+
+
+
+
+
 
 ## Melhor erro ponderaro: 3.49 , th:0.2
 set.seed(1)
@@ -65,14 +79,70 @@ melhor = melhorIndice(thr)
 regfit.best=regsubsets(diabetesB~.,data=dados2,nvmax=19)
 summary(regfit.best)
 
-## 1p - Melhor erro ponderaro: 3.51 , th:0.4
+
+
+## 1p - Melhor erro ponderaro: 3.55 , th:0.4
 set.seed(1)
-thr = graficoThreshold(0,1,0.1,glmCV,diabetesB~stab.glu,dados2)
+thr = graficoThreshold(0,1,0.1,glmCV,diabetesB~poly(stab.glu,3),dados2)
 melhor = melhorIndice(thr)
 
-## 2p -Melhor erro ponderaro: 3.516 , th:0.2
+## 1p - Melhor erro ponderaro: 3.55 , th:0.4
+set.seed(1)
+thr = graficoThreshold(0,1,0.01,glmCV,diabetesB~poly(stab.glu,2),dados2)
+melhor = melhorIndice(thr)
+thr[40,]
+
+
+
+
+
+## 1p - Melhor erro ponderaro: 3.55 , th:0.4
+set.seed(1)
+thr = graficoThreshold(0,1,0.1,glmCV,diabetesB~stab.glu+ratio+stab.glu:ratio,dados2)
+melhor = melhorIndice(thr)
+
+
+## 1p - Melhor erro ponderaro: 3.56 , th:0.4
+set.seed(1)
+thr = graficoThreshold(0,1,0.01,glmCV,diabetesB~stab.glu+ratio+stab.glu:ratio,dados2)
+melhor = melhorIndice(thr)
+
+## 1p - Melhor erro ponderaro: 3.55 , th:0.4 anterior com 0.0.1 th
+set.seed(1)
+thr = graficoThreshold(0,1,0.01,glmCV,diabetesB~stab.glu+ratio+stab.glu:ratio,dados2)
+melhor = melhorIndice(thr)
+
+## 1p - Melhor erro ponderaro: 3.57 , th:0.4 Stab.glu + ratio
+set.seed(1)
+thr = graficoThreshold(0,1,0.01,glmCV,diabetesB~poly(stab.glu,2)+ratio,dados2)
+melhor = melhorIndice(thr)
+thr[40,]
+
+## 1p - Melhor erro ponderaro: 3.59 , th:0.4 Stab.glu + ratio
+set.seed(1)
+thr = graficoThreshold(0,1,0.01,glmCV,diabetesB~poly(stab.glu,2)+ratio+age,dados2)
+melhor = melhorIndice(thr)
+thr[40,]
+
+
+## 2p -Melhor erro ponderaro: 3.52 , th:0.2
+set.seed(1)
+thr = graficoThreshold(0,1,0.1,glmCV,diabetesB~stab.glu+ age +stab.glu:age ,dados2)
+melhor = melhorIndice(thr)
+
+
+## 2p -Melhor erro ponderaro: 3.51 , th:0.2
 set.seed(1)
 thr = graficoThreshold(0,1,0.1,glmCV,diabetesB~stab.glu+ratio,dados2)
+melhor = melhorIndice(thr)
+
+## 2p -Melhor erro ponderaro: 3.56 , th:0.2
+set.seed(1)
+thr = graficoThreshold(0,1,0.01,glmCV,diabetesB~stab.glu+ratio,dados2)
+melhor = melhorIndice(thr)
+
+set.seed(1)
+thr = graficoThreshold(0,1,0.01,glmCV,diabetesB~stab.glu+ratio,dados2)
 melhor = melhorIndice(thr)
 
 ## 3p - Melhor erro ponderaro: 3.52 , th:0.4
