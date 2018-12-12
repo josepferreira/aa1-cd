3f2bb8bc3c932783029317efdc691e4de4359fdc
José Pedro Ferreira <zepedroferreira11@gmail.com>
2018-12-12 09:49:23 +0000

    Replace testeQDAeLDA.R

--- Parte 2/Selecção do modelo/testeQDAeLDA.R
+++ Parte 2/Selecção do modelo/testeQDAeLDA.R
@@ -1,5 +1,5 @@
 
-
+library(MASS)
 library(class)
 
 library(dplyr)
@@ -10,14 +10,21 @@ treino = treino[complete.cases(treino),]
 
 qda1 = graficoThreshold(0,1,0.02,qdaCV,diabetesB~age,treino)
 melhorQ1 = melhorIndice(qda1)
-## acerto muito baixo em torno dos 70%
+## acerto muito baixo em torno dos 71%
 
-treino = dplyr::select(nossoDiabetes, diabetesB, age, waist)
+treino = dplyr::select(nossoDiabetes, diabetesB, age, bp.1s)
 treino = treino[complete.cases(treino),]
 
-qda2 = graficoThreshold(0,1,0.02,qdaCV,diabetesB~age+waist,treino)
+qda2 = graficoThreshold(0,1,0.02,qdaCV,diabetesB~age+bp.1s,treino)
 melhorQ2 = melhorIndice(qda2)
 ## acerto muito baixo em torno dos 71%
+
+treino = dplyr::select(nossoDiabetes, diabetesB, age, bp.1s, waist)
+treino = treino[complete.cases(treino),]
+
+qda3 = graficoThreshold(0,1,0.02,qdaCV,diabetesB~age+bp.1s+waist,treino)
+melhorQ3 = melhorIndice(qda3)
+## acerto muito baixo em torno dos 72,25%
 
 
 #### LDA
@@ -26,12 +33,19 @@ treino = treino[complete.cases(treino),]
 
 lda1 = graficoThreshold(0,1,0.02,ldaCV,diabetesB~age,treino)
 melhorL1 = melhorIndice(lda1)
-## acerto na ordem dos 70%
+## acerto na ordem dos 71,75%
 
-treino = dplyr::select(nossoDiabetes, diabetesB, age, waist)
+treino = dplyr::select(nossoDiabetes, diabetesB, age, bp.1s)
 treino = treino[complete.cases(treino),]
 
-lda2 = graficoThreshold(0,1,0.02,ldaCV,diabetesB~age+waist,treino)
+lda2 = graficoThreshold(0,1,0.02,qdaCV,diabetesB~age+bp.1s,treino)
 melhorL2 = melhorIndice(lda2)
-## acerto muito baixo em torno dos 73%
+## acerto muito baixo em torno dos 71,75%
+
+treino = dplyr::select(nossoDiabetes, diabetesB, age, bp.1s, waist)
+treino = treino[complete.cases(treino),]
+
+lda3 = graficoThreshold(0,1,0.02,qdaCV,diabetesB~age+bp.1s+waist,treino)
+melhorL3 = melhorIndice(lda3)
+s## acerto muito baixo em torno dos 72,25%
 
