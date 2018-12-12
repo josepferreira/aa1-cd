4ac223821055e801121424848aac43b3ddda2375
José Pedro Ferreira <zepedroferreira11@gmail.com>
2018-12-12 20:28:53 +0000

    Replace testeQDAeLDA.R

--- Parte 2/Selecção do modelo/testeQDAeLDA.R
+++ Parte 2/Selecção do modelo/testeQDAeLDA.R
@@ -17,6 +17,7 @@ treino = treino[complete.cases(treino),]
 
 qda2 = graficoThreshold(0,1,0.02,qdaCV,diabetesB~age+bp.1s,treino)
 melhorQ2 = melhorIndice(qda2)
+qda2[12,]
 ## acerto muito baixo em torno dos 71%
 
 treino = dplyr::select(nossoDiabetes, diabetesB, age, bp.1s, waist)
@@ -24,6 +25,7 @@ treino = treino[complete.cases(treino),]
 
 qda3 = graficoThreshold(0,1,0.02,qdaCV,diabetesB~age+bp.1s+waist,treino)
 melhorQ3 = melhorIndice(qda3)
+thr[14,]
 ## acerto muito baixo em torno dos 72,25%
 
 
@@ -38,14 +40,14 @@ melhorL1 = melhorIndice(lda1)
 treino = dplyr::select(nossoDiabetes, diabetesB, age, bp.1s)
 treino = treino[complete.cases(treino),]
 
-lda2 = graficoThreshold(0,1,0.02,qdaCV,diabetesB~age+bp.1s,treino)
+lda2 = graficoThreshold(0,1,0.02,ldaCV,diabetesB~age+bp.1s,treino)
 melhorL2 = melhorIndice(lda2)
 ## acerto muito baixo em torno dos 71,75%
 
 treino = dplyr::select(nossoDiabetes, diabetesB, age, bp.1s, waist)
 treino = treino[complete.cases(treino),]
 
-lda3 = graficoThreshold(0,1,0.02,qdaCV,diabetesB~age+bp.1s+waist,treino)
+lda3 = graficoThreshold(0,1,0.02,ldaCV,diabetesB~age+bp.1s+waist,treino)
 melhorL3 = melhorIndice(lda3)
-s## acerto muito baixo em torno dos 72,25%
+## acerto muito baixo em torno dos 72,25%
 
